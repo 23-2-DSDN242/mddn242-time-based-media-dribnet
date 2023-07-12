0f16373ab015400bba98dfdff6fafc746154bb55
tuakana-vuw <71419542+tuakana-vuw@users.noreply.github.com>
2023-07-13 11:04:40 +1200

    Updated maeda clock.

--- maeda_clock.js
+++ maeda_clock.js
@@ -1,9 +1,15 @@
 // Update this function to draw you own maeda clock on a 960x500 canvas
 function draw_clock(obj) {
   // YOUR MAIN CLOCK CODE GOES HERE
-  background(50); //  beige
-  fill(200); // dark grey
-  textSize(40);
-  textAlign(CENTER, CENTER);
-  text("YOUR MAEDA CLOCK CODE GOES HERE", width/2, height/2);
+  background(0); //  black
+  fill(255); // white
+
+  // this the number one
+  rect(100, 100, 50, 300);
+
+  // this is the number zero
+  rect(200, 100, 50, 300);
+  rect(300, 100, 50, 300);
+  rect(200, 100, 100, 30);
+  rect(200, 370, 100, 30);
 }
